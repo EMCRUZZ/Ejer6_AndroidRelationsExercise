595abe97984a6350026e40831355a1f594ed3b5f
Emmanuel <emmanuel@emmanuel.es>
2020-01-08 10:15:25 +0100

    Ejer1 completado.

--- Ejer6_AndroidRelations/MainActivity.cs
+++ Ejer6_AndroidRelations/MainActivity.cs
@@ -6,13 +6,14 @@ using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
+using AlertDialog = Android.App.AlertDialog;
 
 namespace Ejer6_AndroidRelations
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-
+        EditText inputTexto;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -23,6 +24,21 @@ namespace Ejer6_AndroidRelations
 
             FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
             fab.Click += FabOnClick;
+            
+            //Mis referencias de ids
+            Button idboton = FindViewById<Button>(Resource.Id.idboton);
+            idboton.Click += Idboton_Click;
+            inputTexto = FindViewById<EditText>(Resource.Id.inputTexto);
+        
+        }
+        //metodo del boton
+        private void Idboton_Click(object sender, EventArgs e)
+        {
+            //ventana emergente
+            AlertDialog.Builder ventanaEmergente = new AlertDialog.Builder(this);
+            ventanaEmergente.SetTitle("Ejer1");
+            ventanaEmergente.SetMessage(inputTexto.Text.ToString());
+            ventanaEmergente.Show();
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
@@ -41,11 +57,10 @@ namespace Ejer6_AndroidRelations
 
             return base.OnOptionsItemSelected(item);
         }
-
         private void FabOnClick(object sender, EventArgs eventArgs)
         {
             View view = (View) sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+            Snackbar.Make(view, "Replace with ff", Snackbar.LengthLong)
                 .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
         }
 	}
