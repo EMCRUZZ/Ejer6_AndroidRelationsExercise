1c5baa65f27f70dcae9cf3e0cb35f92b32f10143
Emmanuel <emmanuel@emmanuel.es>
2020-01-08 19:34:10 +0100

    Ejer4 Falta la navegación hacia la ventana de pin correcto y los findviewbyid siguen fallando igual menos el primero...

--- Ejer6_AndroidRelations/MainActivity.cs
+++ Ejer6_AndroidRelations/MainActivity.cs
@@ -12,7 +12,7 @@ namespace Ejer6_AndroidRelations
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-
+        EditText inputTexto;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -23,8 +23,42 @@ namespace Ejer6_AndroidRelations
 
             FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
             fab.Click += FabOnClick;
+
+            //Mis ID
+
+            inputTexto = FindViewById<EditText>(Resource.Id.inputTexto);
+
+            Button idboton = FindViewById<Button>(Resource.Id.idboton1);
+            idboton.Click += Idboton_Click;
+
+            /* Al igual que los otros finds este tampoco me encuentra el id en el resource.id 
+             
+            var idbotonOK = FindViewById<Button>(Resource.Id.idbotonOK);
+            idboton.Click += IdbotonOK_Click;
+            */
+
+            //he omitido de esta rama los otros FindViewById comentados mientras que no sepa el porque no funcionan.
         }
 
+        private void IdbotonOK_Click(object sender, EventArgs e)
+        {
+            if (inputTexto.Text.ToString().Equals("1234"))
+            {
+                //Creo y levanto la siguiente pantalla de codigo correcto
+            }
+            else
+            {
+                //cambio el color a rojo si esta mal
+                inputTexto.SetTextColor(Android.Graphics.Color.Red);
+            }
+        }
+
+        private void Idboton_Click(object sender, EventArgs e)
+        {
+            //obtengo el numero de boton que sea pulsado para poder reutilizar la función
+            Button temp = (Button)sender;
+            inputTexto.Text = inputTexto.Text.ToString() + temp.Text.ToString();
+        }
         public override bool OnCreateOptionsMenu(IMenu menu)
         {
             MenuInflater.Inflate(Resource.Menu.menu_main, menu);
